86ac42051fe8ae068f44d4306ffc4582cb4ca1b1
E233187 <E233187@CSS-D0051351>
2016-05-31 10:53:13 -0400

    QWidgets completed and tested

---
+++ Pluralsight/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pydownloader',
+    version='1.0',
+    description='using pydownloader to test site-package module insertion',
+    author="Rob Hoover",
+    author_email='hoovajr1@yahoo.com',
+    py_modules=['pydownloader'],
+    )
---
+++ qwidget-demos/qwidget-demos/qwidget_demos.py
@@ -0,0 +1,39 @@
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+import sys
+
+
+
+class QWidgetDemos(QDialog):
+    def __init__(self):
+        QDialog.__init__(self)
+        self.setWindowTitle("QWidgets Demo")
+
+        self.combobox = QComboBox()
+      
+        self.combobox.addItems(["Item 1", "Item 2", "Item 3"])
+
+        close_button = QPushButton("Close")
+        close_button.clicked.connect(self.close)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.combobox)
+        layout.addWidget(close_button)
+        
+        self.setLayout(layout)
+        
+        self.setFocus()
+        self.combobox.currentIndexChanged.connect(self.selected)
+        
+
+    def selected(self):
+        current_text = self.combobox.currentText()
+        current_index = str(self.combobox.currentIndex())
+
+        print(current_text + " at the index " + current_index)
+
+app = QApplication(sys.argv)
+dialog = QWidgetDemos()
+dialog.show()
+app.exec_()
+
